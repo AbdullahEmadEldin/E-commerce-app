2f4623a49d56fad68624204527419f95abe8508d
Abdullah Emad <77693593+Master-Abdullah@users.noreply.github.com>
2023-07-13 21:42:22 +0300

    create the project
    add all files of the project to project folder

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EcommerceApp());
+}
+
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
