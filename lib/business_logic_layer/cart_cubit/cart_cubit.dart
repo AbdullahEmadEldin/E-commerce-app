0fd34ca75fac1f25a4159a296e96ebe9ee3f544a
Abdullah Emad <amenesy8@gmail.com>
2023-10-27 17:48:37 +0200

    Add features of cart - make logic of adding quantity of the same product not adding a new tile of it - delete product from cart

--- lib/business_logic_layer/cart_cubit/cart_cubit.dart
+++ lib/business_logic_layer/cart_cubit/cart_cubit.dart
@@ -14,17 +14,56 @@ class CartCubit extends Cubit<CartState> {
   StreamSubscription<List<UserProduct>>? cartStreamSubscription;
   CartCubit({required this.cartRepository}) : super(CartInitial());
 
-  Future<void> addToCart(Product product, String? dropdownValue) async {
+  List<UserProduct> cartProducts = [];
+  Future<void> addToCart(
+      {required Product product, String? dropdownValue}) async {
     try {
-      final userProduct = UserProduct(
+      UserProduct? userProduct;
+      cartProducts = await cartRepository.myCart().first;
+      if (cartProducts.isEmpty) {
+        userProduct = UserProduct(
           id: kIdFromDartGenerator(),
+          productId: product.productID,
           color: 'color',
           size: dropdownValue ?? 'size',
-          productID: product.productID,
           title: product.title,
           price: product.price,
-          imgUrl: product.imgUrl);
-      await cartRepository.addToCart(userProduct);
+          imgUrl: product.imgUrl,
+          discount: product.discount,
+        );
+        print('000000000000000000000000000=====>>>${product.discount}');
+        await cartRepository.addToCart(userProduct);
+      } else {
+        int index = -1;
+        for (int i = 0; i < cartProducts.length; i++) {
+          print(
+              'boool:stored productId: ${cartProducts[i].productId}  new one:  ${product.productID}');
+          if (cartProducts[i].productId == product.productID) {
+            index = i;
+          }
+          if (index != -1) {
+            await cartRepository.addToCart(cartProducts[i]
+                .compywith(quantity: cartProducts[i].quantity + 1));
+            print('second case');
+            break;
+          } else if (index == -1 && i == cartProducts.length - 1) {
+            userProduct = UserProduct(
+              id: kIdFromDartGenerator(),
+              productId: product.productID,
+              color: 'color',
+              size: dropdownValue ?? 'size',
+              title: product.title,
+              price: product.price,
+              imgUrl: product.imgUrl,
+              discount: product.discount,
+            );
+            await cartRepository.addToCart(userProduct);
+            print('000000000000000000000000000=====>>>${product.discount}');
+            print('third case');
+          }
+        }
+      }
+      print('${cartProducts.length}');
       print('Sucessssssss added to cart');
       emit(SucessAddToCart());
     } catch (e) {
@@ -33,9 +72,26 @@ class CartCubit extends Cubit<CartState> {
     }
   }
 
+  Future<void> deleteFromCart(UserProduct product) async {
+    try {
+      cartRepository.deleteCartProduct(userProduct: product);
+    } catch (e) {
+      print('error on deleting cart product:: ${e.toString}');
+    }
+  }
+
+  Future<void> cartProductQuantity(
+      {required UserProduct userProduct, required int quantity}) async {
+    try {
+      cartRepository.addToCart(userProduct.compywith(quantity: quantity));
+    } catch (e) {
+      print('error on modifying quantity');
+    }
+  }
+
   Future<void> getCartProducts() async {
     emit(LoadingCartProducts());
-    cartStreamSubscription = cartRepository.myBag().listen((cartProducts) {
+    cartStreamSubscription = cartRepository.myCart().listen((cartProducts) {
       print('Geeeeeeeeeeeeeeeeet Cart products doneeee');
       emit(SuccessCartProducts(cartProducts: cartProducts));
     }, onError: (error) {
